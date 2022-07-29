25079a6eb4f9f07ae30b2b852df973f8af68ec19
niravrathod <niravrathod473@gmail.com>
2022-07-29 17:56:19 +0530

    1 file modified

--- src/server.js
+++ src/server.js
@@ -7,3 +7,22 @@ let timerId = null,
 var tradedata = require('./data');
 
 app.use(express.static(__dirname + '/dist'));
+
+io.on('connection', socket => {
+
+  console.log(`Socket ${socket.id} added`);
+  localdata = tradedata.data;
+  sockets.add(socket);
+  if (!timerId) {
+      startTimer();
+  }
+  socket.on('clientdata', data => {
+      console.log(data);
+  });
+  socket.on('disconnect', () => {
+      console.log(`Deleting socket: ${socket.id}`);
+      sockets.delete(socket);
+      console.log(`Remaining sockets: ${sockets.size}`);
+  });
+
+});
